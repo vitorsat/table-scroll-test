9609c0e1f5a09585f6e6baed89e380423784abd8
VitorMiguel <vitor.miguel0581@gmail.com>
2022-09-13 17:25:21 -0300

    feat: add tailwand and refactor docs

---
+++ src/app/routes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
+
+import { Login } from "./pages/Login/Login";
+import { Main } from "./pages/Main/Main";
+
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="login" element={<Login />} />
+        <Route path="main" element={<Main />} />
+        <Route path="/" element={<Login />} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
