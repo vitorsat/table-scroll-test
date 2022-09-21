a9cf528fb6959a5fb0f4aa1d979afcf783d34337
VitorMiguel <vitor.miguel0581@gmail.com>
2022-09-21 15:52:05 -0300

    feat: add error

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { useListProducts } from "../hooks/useListProducts";
 import { Loader } from "./Loader";
+import { BiError } from "react-icons/bi";
+
 
 let renderCount = 0
 
@@ -8,27 +10,34 @@ export function ListComponent() {
   renderCount++
   const { getListProducts } = useListProducts()
 
-  if (getListProducts.isLoading) {
-    return <Loader />
-  }
-
   return (
     <>
+      {getListProducts.isLoading ? <Loader /> : ''}
       <h1 className="text-fuchsia-50 flex align-center justify-center mt-3">
         Render Count: {renderCount}
       </h1>
-      <div className="text-center mt-10">
-        <ul>List</ul>
-        {getListProducts.data?.map((item: any) => {
-          return (
-            <div key={item.id}>
-              <Link to={`/products/${item.id}`}>
-                <li className="pb-3">{item.name}</li>
-              </Link>
-            </div>
-          )
-        })}
-      </div>
+      {getListProducts.error ?
+        <>
+          <h1 className="text-center mt-10">List</h1>
+          <div className="text-center flex flex-row align-middle justify-center mt-96">
+            <BiError className="text-6xl text-red-500" />
+            <p className="ml-4 text-center mt-3 text-4xl">{getListProducts.error?.message}</p>
+          </div>
+        </>
+        :
+        <div className="text-center mt-10">
+          <ul>List</ul>
+          {getListProducts.data?.map((item: any) => {
+            return (
+              <div key={item.id}>
+                <Link to={`/products/${item.id}`}>
+                  <li className="pb-3">{item.name}</li>
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+      }
     </>
   );
 }
